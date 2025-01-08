cb66c51a79e32411838699b67a2f6f6b86592b06
muhaiminverse <muhaiminverse@gmail.com>
2025-01-08 11:03:58 +0600

    request validation backend operations

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -33,7 +33,7 @@ export const jwtParse = async(req: Request, res: Response, next: NextFunction) =
     const decoded = jwt.decode(token) as jwt.JwtPayload;
     const auth0Id = decoded.sub;
 
-    const user = await User.findOne({auth});
+    const user = await User.findOne({auth0Id});
 
     if(!user){
       res.sendStatus(401);
