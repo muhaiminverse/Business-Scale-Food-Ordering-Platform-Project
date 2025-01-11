2c2b1141a86174ec398db83f03ae03b4d7fcdbae
muhaiminverse <muhaiminverse@gmail.com>
2025-01-11 18:32:56 +0600

    GET request for pre-populating the form values

--- frontend/src/api/MyUserApi.tsx
+++ frontend/src/api/MyUserApi.tsx
@@ -1,12 +1,44 @@
+import { User } from "@/types";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useMutation } from "react-query";
+import { useMutation, useQuery } from "react-query";
+import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+
+export const useGetMyUser = () => {
+    const { getAccessTokenSilently } = useAuth0();
+
+    const getMyUserRequest = async (): Promise<User> => {
+        const accessToken = await getAccessTokenSilently();
+
+        const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                "Content-Type": "application/json",
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to get user');
+        };
+
+        return response.json();
+    };
+
+    const { data:currentUser, isLoading, error } = useQuery('fetchCurrentUser', getMyUserRequest);
+
+    if(error){
+        toast.error(error.toString());
+    }
+
+    return { currentUser, isLoading };
+};
 
 type CreateUserRequest = {
     auth0Id: string;
     email: string;
-}
+};
 
 export const useCreateMyUser = () => {
     const {getAccessTokenSilently} =  useAuth0();
@@ -47,7 +79,6 @@ type UpdateUserRequest = {
 export const useUpdateMyUser = () => {
     const { getAccessTokenSilently } = useAuth0();
 
-
     const updateMyUserRequest = async (formData: UpdateUserRequest) => {
         const accessToken = await getAccessTokenSilently();
         const response = await fetch(`${API_BASE_URL}/api/my/user`, {
@@ -69,10 +100,20 @@ export const useUpdateMyUser = () => {
     const {
         mutateAsync: updateUser,
         isLoading,
-        // isSuccess,
-        // isError,
-        // error, reset,
+        isSuccess,
+        error, 
+        reset,
     } = useMutation(updateMyUserRequest);
+
+    if(isSuccess){
+        toast.success('User updated successfully');
+    }
+    
+    if(error){
+        toast.error(error.toString());
+        reset();
+    }
+
 
     return { updateUser, isLoading };
 };
