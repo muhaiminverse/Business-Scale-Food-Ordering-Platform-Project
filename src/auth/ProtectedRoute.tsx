6ff1a5fdb237b517d1f34d626c5b444cba8019f4
muhaiminverse <muhaiminverse@gmail.com>
2025-02-09 18:08:08 +0600

    homepage redirect bug error fix 1.1

--- src/auth/ProtectedRoute.tsx
+++ src/auth/ProtectedRoute.tsx
@@ -2,18 +2,18 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = () => {
-  const { isAuthenticated, isLoading} = useAuth0();
+  const { isAuthenticated, isLoading } = useAuth0();
 
-  if(isLoading){
+  if (isLoading) {
     return null;
   }
 
-  if(isAuthenticated){
-    return <Outlet/>;
+  if (isAuthenticated) {
+    return <Outlet />;
   }
-
-  return <Navigate to="/" replace/>
-
+  else {
+    return <Navigate to="/" replace />
+  }
 
   // 8:09:56 return isAuthenticated ? (<Outlet/>) : (<Navigate to="/" replace/>);
 };
